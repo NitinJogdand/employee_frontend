95b6459439a02cb746daa12b9602252ee19aa070
NitinJogdand <nitinJogdand071@gmail.com>
2024-02-22 17:02:36 +0530

    commit-43

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from '../service/employee.service';
+import { EmployeeService, InputEmployee } from '../service/employee.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
+
+
 export class MainComponent implements OnInit {
 
   employees:any[]=[];
   showTable:boolean = false;
+  addRecord:boolean = false;
+  updateRecord:boolean = false;
+  forUpdate:boolean = false
+  showSave:boolean = true
+  inputEmpolyee:InputEmployee = new InputEmployee();
+  
   constructor(private employeeService:EmployeeService) { }
 
   ngOnInit(): void {
@@ -23,6 +31,58 @@ export class MainComponent implements OnInit {
       console.log("Something went wrong..")
     })
     this.showTable = true
+    this.addRecord = false
+    this.updateRecord = false
+  }
+
+  addRecordOption(){
+    this.forUpdate = false
+    this.updateRecord = false
+    this.inputEmpolyee = new InputEmployee();
+    this.showTable = false
+    this.addRecord = true
+   
+
+    
+  }
+
+  savedata(){
+    console.log(this.inputEmpolyee)
+    this.employeeService.saveEmployee(this.inputEmpolyee).subscribe((data)=>{
+      console.log(data)
+    },(error)=>{
+      console.log(error)
+    })
+  }
+
+  deleteRecord(deleteRecord:any){
+    let id = deleteRecord.id;
+    this.employeeService.deleteRecord(id).subscribe((data)=>{
+      console.log(data)
+    },(error)=>{
+      console.log(error)
+    })
+  }
+
+  updateRecordClick(data:any){
+    this.showTable = false
+    this.addRecord = true
+    this.updateRecord = false
+    this.forUpdate =true
+    this.forUpdate = false
+    this.inputEmpolyee.employeeName = data.employeeName;
+    this.inputEmpolyee.employeeAge = data.employeeAge;
+    this.inputEmpolyee.employeeMobailNumber = data.employeeMobailNumber;
+    this.inputEmpolyee.employeeSalary = data.employeeSalary;
+    this.inputEmpolyee.employeeCompany = data.employeeCompany;
+    this.inputEmpolyee.employeeLivingCity = data.employeeLivingCity;
+    this.inputEmpolyee.employeeNationality = data.employeeNationality;
+
+    console.log(this.inputEmpolyee)
+  }
+
+  updateRecordSave(){
+
   }
 
   hidAll(){
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -9,6 +9,27 @@ export class EmployeeService {
   constructor(private http:HttpClient) { }
 
   getAllEmployeeList(){
-    return this.http.get<any>('http://localhost:8081/employee/employee/getAll');
+    return this.http.get<any>('http://localhost:9090/employee/getAll');
   }
+
+  saveEmployee(inputEmployee:InputEmployee){
+    return this.http.post('http://localhost:9090/employee/save',inputEmployee)
+  }
+
+  deleteRecord(id:number){
+    return this.http.delete(`http://localhost:9090/employee/deleteEmployee/${id}`)
+  }
+
+  updateEmployee(employee:any){
+    return this.http.put(`http://localhost:9090/employee/update`,employee)
+  }
+}
+export class InputEmployee{
+  employeeName:string='';
+  employeeAge:number=0;
+  employeeMobailNumber:number=0;
+  employeeSalary:number = 0;
+  employeeCompany:string='';
+  employeeLivingCity:string='';
+  employeeNationality:string=''
 }
